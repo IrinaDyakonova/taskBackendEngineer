43d8ca29e4796c0e51f45bda466cce9f7025343b
IrinaDyakonova <dyakonova.irina.v@gmail.com>
2020-09-01 15:27:45 +0300

    first commit

---
+++ src/test/java/taskbackendengineer/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package taskbackendengineer.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
